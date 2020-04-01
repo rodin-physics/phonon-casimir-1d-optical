5097e74bb6835b19a21ff5787b8556c0e84e67b4
Aleksandr Rodin <alerodin@gmail.com>
2020-04-01 17:12:33 +0800

    QFT energy calculation at zero-T. Seems to agree with ED

--- src/general.jl
+++ src/general.jl
@@ -4,8 +4,9 @@ using Plots
 using QuadGK
 
 ## Parameters
-const ν = 1e-3;         # Relative tolerance for integration
+const ν = 1e-4;         # Relative tolerance for integration
 const η = 1e-7;         # Small number used for i0
+const α = 1e-9;         # Small number for absolute tolerance
 const NumEvals = 1e7;   # Maximum number of evaluations in quadgk
 
 # Impurity type
@@ -85,14 +86,14 @@ function Ξ_jl(z, system, j, l)
 
     mj = system.Ms[Imp_j.n]
     ml = system.Ms[Imp_l.n]
-    D = Imp_j.pos - Imp_l.pos |> abs
+    D = Imp_j.pos - Imp_l.pos
 
     f_int(θ) =
         -√(mj * ml) *
         Ξ_jl_integrand(z, system.Ms, θ, Imp_j.n, Imp_l.n) *
         exp(1im * θ * D)
 
-    res = quadgk(f_int, 0, 2 * π)[1]
+    res = quadgk(f_int, 0, 2 * π, atol = α)[1]
     return (res / (2 * π))
 end
 
@@ -106,107 +107,23 @@ end
 
 # Interaction energy (for now computed at T = 0)
 # FI Integrand for zero-T energy
-function F_I_Integrand_T0(system, z)
+function F_I_Integrand_T0(z, system)
     nImps = length(system.Imps)
     Ξ_ = Ξ(z, system)
     λs = map(x -> x.λ, system.Imps)
     ms = map(x -> system.Ms[x.n], system.Imps)
     α = Diagonal(1 ./ ms .- 1 ./ λs)
     Δ = (Matrix{Int}(I, nImps, nImps) .+ Ξ_ * α)
-    Ξ0 = map(j -> Ξ_jl(z, system, j, j), 1:nImps) |> Diagonal
-    Δ0_Inv = (Matrix{Int}(I, nImps, nImps) .+ Ξ0 * α) |> inv
-
+    Δ0_Inv = diag(Δ) |> Diagonal |> inv
     return (det(Δ0_Inv * Δ) |> Complex |> log |> real)
 end
 
 # FI for zero T
 function F_I_T0(system)
-    f(w) = F_I_Integrand_T0(system, 1im * w)
-    res = quadgk(f, -Inf, 0, Inf, maxevals = NumEvals)[1]
-    return (res / (2 * π))
+    f(w) = F_I_Integrand_T0(1im * w, system)
+    res = quadgk(f, -Inf, 0, Inf, maxevals = NumEvals, rtol = ν)[1]
+    return (res / (2 * π) / 2) # See the F_I formula for the division by 2
 end
-
-## TODO
-function Q2_sθ(Ms, s, θ)
-    m = Ms[1]
-    M = Ms[2]
-    # s should be 1 for optical branch, 0 for acoustic branch
-    # - eventually generalise to n branches
-    sqTerm = sqrt(m^2 + M^2 + 2 * m * M * cos(θ)) / (m * M)
-    invariantTerm = (m + M) / (m * M)
-    if s == 0
-        return (invariantTerm - sqTerm)
-    elseif s == 1
-        return (invariantTerm + sqTerm)
-    else
-        error("This branch is not available at the moment")
-    end
-end
-
-# Unnormalized mode vector
-function e_sθ(Ms, s, θ)
-    m = Ms[1]
-    M = Ms[2]
-    return ([1 + exp(-1im * θ), 2 - m * Q2_sθ([m, M], s, θ)])
-end
-
-# Mode couling term
-function coupling(Ms, s, θ)
-    e = e_sθ(Ms, s, θ)
-    return (Diagonal(Ms) * e * e' * Diagonal(Ms)) / (e' * Diagonal(Ms) * e)
-end
-
-# Impurity coupling
-function YGY(Ms, z, D, nj, nk)
-    f_int_acc(θ) =
-        Q2_sθ(Ms, 0, θ) * exp(1im * D * θ) / (-z^2 + Q2_sθ(Ms, 0, θ)) *
-        (coupling(Ms, 0, θ)[nj, nk])
-
-    f_int_opt(θ) =
-        Q2_sθ(Ms, 1, θ) * exp(1im * D * θ) / (-z^2 + Q2_sθ(Ms, 1, θ)) *
-        (coupling(Ms, 1, θ)[nj, nk])
-
-    f_int(θ) = f_int_opt(θ) + f_int_acc(θ)
-    res = quadgk(f_int, 0, 2 * π)[1]
-    return (-res) / (2 * π)
-end
-
-function Δ(Ms, z, Imps)
-    nImps = length(Imps)
-    α = map(x -> 1 / Ms[x.n] - 1 / x.λ, Imps) |> Diagonal
-    Imp_Mat = repeat(Imps, 1, nImps)    # Impurity position matrix
-    ImpT_Mat = permutedims(Imp_Mat)     # Transpose of impurity position matrix
-    YGY_ = map(
-        (x, y) -> YGY(m, M, z, abs.(x.pos - y.pos), x.n, y.n),
-        Imp_Mat,
-        ImpT_Mat,
-    )
-
-
-    return (Matrix{Int}(I, nImps, nImps) .+ Prop_Mat * α)
-end
-
-function FI_Integrand(Ms, z, Imps)
-    α = map(x -> 1 / Ms[x.n] - 1 / x.λ, Imps) |> Diagonal
-    Δ_ = Δ(Ms, z, Imps)
-    Δ0_Inv =
-        map(
-            x -> 1 ./ (1 .+ YGY(Ms, z, 0, x.n, x.n) .* (1 / Ms[x.n] - 1 / x.λ)),
-            Imps,
-        ) |> Diagonal
-
-    return det(Δ0_Inv * Δ_) |> Complex |> log |> real
-end
-
-# @time YGY([1, 2], 1.2 + 1im * η, 2, 1, 2)
-# YGY([1, 2], 1.2 + 1im * η, 2, 1, 2)
-# # Q2_sθ([1,2.], 1, 1.2)
-#
-#
-# exact_F([1], 40, [Impurity(1, 1, 2.0), Impurity(4, 1, 2.0)], 1e-2)
-# exact_F([1, 1], 20, [Impurity(1, 1, 2.0), Impurity(2, 2, 2.0)], 1e-2)
-# Exact_Free_Energy(5, 2.0, 2.0, 1, 2 * 1e-2) * 2
-
 
 # Colors for plotting
 my_red = RGB(215 / 255, 67 / 255, 84 / 255)
