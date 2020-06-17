f2bc8f05f323be2ecec69fd126bb45e50611ea51
Gideon Lee <Gideon@users-MacBook-Pro-6.local>
2020-06-17 19:11:44 +0800

    Update variousplots.jl
    some diatomic plots

--- Test/variousplots.jl
+++ Test/variousplots.jl
@@ -18,21 +18,6 @@ diMs = [1, 3, 12, 50]
 r_1 = 1 ./ Ds
 r_3 = 1 ./ (Ds.^3)
 
-# Comparing for the 0 temperature case
-
-Ds = 2:50
-Ds_Exact = 2 : 3 : 50   # Distances used for the exact diagonalization
-N = 1000                # Chain length for the exact diagonalization
-# T in units of Ω0
-T = 1e-12;
-Ds = Ds_Exact
-# Impurity masses in units of m (chain masses)
-Ms = [1/3, 4/3, 4, 10, 1000]
-
-# Bounding power laws
-r_1 = 1 ./ Ds
-r_3 = 1 ./ (Ds.^3)
-
 ## Plotting
 pyplot()
 plot(
@@ -69,34 +54,72 @@ end
 
 jjkkii = Array{Array{Float64}}(undef, 4, 4, 5)
 fill!(jjkkii, [])
-
+jjkkiiED = Array{Array{Float64}}(undef, 4, 4, 5)
+fill!(jjkkiiED, [])
 
 @time begin
-jj = 4
-kk = 4
+for jj = 2:3
+    print("jj", jj)
+    for kk = jj:4
+        print("kk", kk)
+        imax = length(Ms)
+        ## kk is the heavier atom
+        for ii = 1:(imax - 0)
+            println("ii", ii)
+            diM = [diMs[jj], diMs[kk]];
+            M = Ms[ii];
+
+            # QFT
+            F0 = F_I_T0(System(diM, two_diImps(1, M), T, N))
+            r =  map(x -> F_I_T0(System(diM, two_diImps(x, M), T, N)), Ds_Exact) ./ F0;
+            jjkkii[jj, kk, ii] = r
+            println("QFT Done")
+            # ED exact_FI(s
+            # F0ED = exact_FI(System(diM, two_diImps(1, M), T, N))
+            # rED =  map(x -> exact_FI(System(diM, two_diImps(x, M), T, N)), Ds_Exact) ./ F0ED;
+            # jjkkiiED[jj, kk, ii] = rED
+            # println("ED Done")
+            # Plots.display(plot!(log.(Ds_Exact), real(log.(complex(r))),
+            #     color = colors[ii],
+            #     lab = "",
+            #     markershape = :cross
+            #     ))
+        end
+    end
+end
+
+println("done!")
+end
+
+@time begin
+jj = 2
+kk = 3
 imax = length(Ms)
-# if kk >= 3
-#     imax = length(Ms) - 2
-# end
 ## kk is the heavier atom
-for ii = 1:(imax - 0)
-    println(ii)
+for ii = 3:(imax - 0)
+    println("ii", ii)
     diM = [diMs[jj], diMs[kk]];
     M = Ms[ii];
-    # F_halfway = F_I_T0(System(diM, two_diImps(floor(Int, N/2), M), T, N))
-    F_halfway = 0
-    F0 = F_I_T0(System(diM, two_diImps(1, M), T, N)) - F_halfway
-    r =  map(x -> F_I_T0(System(diM, two_diImps(x, M), T, N)) - F_halfway, Ds_Exact) ./ F0;
+
+    # QFT
+    F0 = F_I_T0(System(diM, two_diImps(1, M), T, N))
+    r =  map(x -> F_I_T0(System(diM, two_diImps(x, M), T, N)), Ds_Exact) ./ F0;
     jjkkii[jj, kk, ii] = r
+    println("QFT Done")
+    # ED exact_FI(s
+    # F0ED = exact_FI(System(diM, two_diImps(1, M), T, N))
+    # rED =  map(x -> exact_FI(System(diM, two_diImps(x, M), T, N)), Ds_Exact) ./ F0ED;
+    # jjkkiiED[jj, kk, ii] = rED
+    # println("ED Done")
     # Plots.display(plot!(log.(Ds_Exact), real(log.(complex(r))),
     #     color = colors[ii],
     #     lab = "",
     #     markershape = :cross
     #     ))
 end
+
 println("done!")
 end
-
 
 plot(
     xaxis = (L"$\ln\, D$", font(14, "Serif")),
@@ -125,12 +148,23 @@ plot!(
     lab = ""
     )
 
-plotData = jjkkii[4, 4, :]
-for ii = 1:(length(Ms) - 0)
-    Plots.display(plot!(log.(Ds_Exact), real(log.(complex(plotData[ii]))),
+jj = 2
+kk = 4
+plotDataQFT = jjkkii[jj, kk, :]
+plotDataED = jjkkiiED[jj, kk, :]
+for ii = 2:(length(Ms) - 1)
+    Plots.display(plot!(log.(Ds_Exact), real(log.(complex(plotDataQFT[ii]))),
         color = colors[ii],
         lab = "",
         markershape = :cross
+        ))
+end
+
+for ii = 2:(length(Ms) - 1)
+    Plots.display(plot!(log.(Ds_Exact), real(log.(complex(plotDataED[ii]))),
+        color = colors[ii],
+        lab = "",
+        markershape = :hexagon
         ))
 end
 
@@ -140,7 +174,18 @@ end
 #     markershape = :cross
 #     ))
 
-savefig("plot50_50_Ms.png")
+
+# Ms = [1/3, 4/3, 4, 10, 1000]
+# diMs = [1, 3, 12, 50]
+savefig("plot_diatomic_EDvQFT_4o3_10.png")
+
+
+using DelimitedFiles
+
+writedlm( "diatomic_QFT_4o3_10_Ds=2-3-50.csv",  plotDataQFT, ',')
+writedlm( "diatomic_ED_4o3_10_Ds=2-3-50.csv",  plotDataED, ',')
+writedlm( "diatomic_jjkkii_tentative",  jjkkii, ',')
+
 
 testArr = real(log.(complex(jjkkii[1, 4, 3])))
 
